742c5b8fd59f8ceba0aadba42bd5388662d2dbe4
IXIOverlorDIXI <andrii.dolhyi@nure.ua>
2023-06-04 23:54:58 +0300

    Lab 4

---
+++ OptimizationLabs.UI/Models/ColorConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace OptimizationLabs.UI.Models
+{
+    public static class ColorConstants
+    {
+        public const string Green = "background-color: #98FB98;";
+        
+        public const string Yellow = "background-color: #FFEA00;";
+    }
+}
